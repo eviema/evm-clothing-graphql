2a42de8a0f0a36684fe8795c1d81e376145b80ad
Evie Yifei Ma <yifei.ma@accenture.com>
2020-11-28 19:53:10 +1100

    integrated redux-persist to store cart state

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,19 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // localStorage
 
 import userReducer from "../redux/user/user.reducer";
 import cartReducer from "../redux/cart/cart.reducer";
 
-export default combineReducers({
-  user: userReducer,
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"], // contains string names/keys of reducers to persist
+};
+
+const rootReducer = combineReducers({
+  user: userReducer, // handled by firebase; no need to persist with redux
   cart: cartReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
