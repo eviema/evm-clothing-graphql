21ab57cc1b1532d619ffb96e1d0eaeb2630540e2
Evie Yifei Ma <yifei.ma189@gmail.com>
2020-12-14 14:15:37 +1100

    removed redux from app

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
 import { ApolloProvider } from "react-apollo";
 import { createHttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { ApolloClient } from "apollo-boost";
 
-import { store, persistor } from "./redux/store";
 import { resolvers, typeDefs } from "./graphql/resolvers";
 import { default as data } from "./graphql/initial-data";
 
@@ -32,15 +29,11 @@ client.writeData({ data });
 
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Provider store={store}>
-      <React.StrictMode>
-        <BrowserRouter>
-          <PersistGate persistor={persistor}>
-            <App />
-          </PersistGate>
-        </BrowserRouter>
-      </React.StrictMode>
-    </Provider>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
   </ApolloProvider>,
   document.getElementById("root")
 );
